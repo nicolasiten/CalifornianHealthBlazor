a47e5e709b9e5d4c284a7cb5fa76e2b722849be5
nicolas.iten <nicolas.iten@enkom.com>
2020-04-10 16:07:59 +0200

    Blazor smooth scrolling

---
+++ CalifornianHealthBlazor/Extensions/BlazorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CalifornianHealthBlazor.Extensions
+{
+    public static class BlazorExtensions
+    {
+        public static ValueTask NavigateToFragmentAsync(this NavigationManager navigationManager, IJSRuntime jSRuntime)
+        {
+            var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+
+            if (uri.Fragment.Length == 0)
+            {
+                return default;
+            }
+            return jSRuntime.InvokeVoidAsync("blazorHelpers.scrollToFragment", uri.Fragment.Substring(1));
+        }
+    }
+}
