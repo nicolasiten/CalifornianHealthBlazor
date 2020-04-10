136dea185d345263395f5bea7fba00bf38782329
nicolas.iten <nicolas.iten@enkom.com>
2020-04-10 21:57:50 +0200

    Added Calendar control

---
+++ CalifornianHealthBlazor/Helpers/BlazorHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace CalifornianHealthBlazor.Helpers
+{
+    public static class BlazorHelpers
+    {
+        public static ValueTask RenderCalendar(IJSRuntime jsRuntime)
+        {
+            return jsRuntime.InvokeVoidAsync("blazorHelpers.renderCalendar");
+        }
+    }
+}
---
+++ CalifornianHealthBlazor/wwwroot/js/blazorHelpers.js
@@ -0,0 +1,34 @@
+﻿(function ($) {
+
+    window.blazorHelpers = {
+        scrollToFragment: (elementId) => {
+            var element = document.getElementById(elementId);
+
+            if (element) {
+                element.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        },
+
+        renderCalendar: () => {
+            $('#evoCalendar').evoCalendar({
+                todayHighlight: true,
+                sidebarToggler: true,
+                eventListToggler: true,
+                eventDisplayDefault: false,
+                canAddEvent: true,
+                calendarEvents: [
+
+                ],
+                onSelectDate: function () {
+                    console.log('onSelectDate!');
+                },
+                onAddEvent: function () {
+                    console.log('onAddEvent!');
+                }
+            });
+        }
+    };
+
+})(jQuery);
