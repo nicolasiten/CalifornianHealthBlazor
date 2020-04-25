018197a3734d1dd0757ef45ebab00b3ba0d32592
nicolas.iten <nicolas.iten@enkom.com>
2020-04-25 20:58:23 +0200

    AppointmentBooking Service Setup

---
+++ src/Services/AppointmentBooking/Amqp/BookingClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppointmentBooking.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace AppointmentBooking.Amqp
+{
+    public class BookingClient : IBookingClient
+    {
+        private readonly IModel _channel;
+        private readonly IBasicProperties _properties;
+
+        private readonly EventingBasicConsumer _consumer;
+        private readonly string _replyQueueName;
+        private string _response;
+
+        public BookingClient(IModel channel)
+        {
+            _channel = channel;
+
+            _replyQueueName = _channel.QueueDeclare().QueueName;
+            _consumer = new EventingBasicConsumer(_channel);
+
+            _properties = _channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            _properties.CorrelationId = correlationId;
+            _properties.ReplyTo = _replyQueueName;
+
+            _consumer.Received += MessageReceived;
+        }
+
+        public string SendBooking(string message)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(string.Empty, "booking_queue", _properties, messageBytes);
+
+            _channel.BasicConsume(_consumer, _replyQueueName, true);
+
+            return _response;
+        }
+
+        private void MessageReceived(object sender, BasicDeliverEventArgs e)
+        {
+            if (e.BasicProperties.CorrelationId == _properties.CorrelationId)
+            {
+                _response = Encoding.UTF8.GetString(e.Body.ToArray());
+            }
+        }
+    }
+}
---
+++ src/Services/AppointmentBooking/Controllers/AppointmentBookingController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppointmentBooking.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace AppointmentBooking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentBookingController : ControllerBase
+    {
+        private readonly IBookingClient _bookingClient;
+
+        public AppointmentBookingController(IBookingClient bookingClient)
+        {
+            _bookingClient = bookingClient;
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
