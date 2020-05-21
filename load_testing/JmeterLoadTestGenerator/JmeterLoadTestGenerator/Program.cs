b50b9200345a35771a7fabefb34801a39445d5b2
nicolasiten <nici.iten@gmail.com>
2020-05-21 23:20:22 +0200

    Created Console app to generate LoadTesting Requests

---
+++ load_testing/JmeterLoadTestGenerator/JmeterLoadTestGenerator/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JmeterLoadTestGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Config
+            var numberOfRequests = 3000;
+            var doctorIds = new[] { 1, 2, 3 };
+            var timeStamps = new[]
+            {
+                "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "13:00", "13:30",
+                "14:00", "14:30", "15:00", "15:30", "16:00", "16:30"
+            };
+            var outputDirectory = $@"C:\Temp\LoadTests\{Guid.NewGuid()}\";
+            var requestNumber = 1;
+
+            if (!Directory.Exists(outputDirectory))
+            {
+                Directory.CreateDirectory(outputDirectory);
+            }
+
+            Console.WriteLine("Start generating request files.");
+
+            foreach (var doctorId in doctorIds)
+            {
+                var date = GetNextWeekday(DateTime.Today);
+                var timeStampIndex = 0;
+
+                for (int i = 0; i < numberOfRequests / doctorIds.Length; i++)
+                {
+                    var request = new Request
+                    {
+                        SelectedConsultantId = doctorId,
+                        SelectedDate = date,
+                        SelectedPatientId = 1,
+                        SelectedTime = timeStamps[timeStampIndex]
+                    };
+
+                    var json = JsonConvert.SerializeObject(request);
+                    File.WriteAllText($"{outputDirectory}{requestNumber}.txt", json);
+                    Console.WriteLine($"Saved request file {requestNumber}");
+
+                    requestNumber++;
+                    timeStampIndex++;
+                    if (timeStampIndex + 1 > timeStamps.Length)
+                    {
+                        date = GetNextWeekday(date);
+                        timeStampIndex = 0;
+                    }
+                }    
+            }
+
+            Console.WriteLine("Start generating request files.");
+        }
+
+        private static DateTime GetNextWeekday(DateTime date)
+        {
+            do
+            {
+                date = date.AddDays(1);
+            } while (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday);
+
+            return date;
+        }
+    }
+}
---
+++ load_testing/JmeterLoadTestGenerator/JmeterLoadTestGenerator/Request.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JmeterLoadTestGenerator
+{
+    public class Request
+    {
+        public int SelectedConsultantId { get; set; }
+
+        public int SelectedPatientId { get; set; }
+
+        public DateTime SelectedDate { get; set; }
+
+        public string SelectedTime { get; set; }
+    }
+}
