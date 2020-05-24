b11f4f4cdba90eaf236f80b5eb8cf1103067e8bf
nicolasiten <nici.iten@gmail.com>
2020-05-24 16:37:10 +0200

    Added Logging to Blazor application

--- src/Web/CalifornianHealthBlazor/Program.cs
+++ src/Web/CalifornianHealthBlazor/Program.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace CalifornianHealthBlazor
 {
@@ -9,6 +13,8 @@ namespace CalifornianHealthBlazor
     {
         public static async Task Main(string[] args)
         {
+            ConfigureLogging();
+
             await CreateHostBuilder(args).Build().RunAsync();
         }
 
@@ -17,6 +23,34 @@ namespace CalifornianHealthBlazor
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
+
+        private static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(ConfigureElasticSink(config, environment))
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            };
+        }
     }
 }
